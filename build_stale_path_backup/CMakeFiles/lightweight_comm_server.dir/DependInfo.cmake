21d8d0c766fec69c416bdd641237e9e74de293e2
tjf-github <tangjianfeng039@gmail.com>
2026-04-20 16:01:07 +0800

    cmake更改

---
+++ build_stale_path_backup/CMakeFiles/lightweight_comm_server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tjf/graduatenew/graduate/src/client_manager.cpp" "CMakeFiles/lightweight_comm_server.dir/src/client_manager.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/client_manager.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/database.cpp" "CMakeFiles/lightweight_comm_server.dir/src/database.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/database.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/file_manager.cpp" "CMakeFiles/lightweight_comm_server.dir/src/file_manager.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/file_manager.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/http_handler.cpp" "CMakeFiles/lightweight_comm_server.dir/src/http_handler.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/http_handler.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/json_helper.cpp" "CMakeFiles/lightweight_comm_server.dir/src/json_helper.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/json_helper.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/logger.cpp" "CMakeFiles/lightweight_comm_server.dir/src/logger.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/logger.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/main.cpp" "CMakeFiles/lightweight_comm_server.dir/src/main.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/main.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/server.cpp" "CMakeFiles/lightweight_comm_server.dir/src/server.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/server.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/session_manager.cpp" "CMakeFiles/lightweight_comm_server.dir/src/session_manager.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/session_manager.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/thread_pool.cpp" "CMakeFiles/lightweight_comm_server.dir/src/thread_pool.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/thread_pool.cpp.o.d"
+  "/home/tjf/graduatenew/graduate/src/user_manager.cpp" "CMakeFiles/lightweight_comm_server.dir/src/user_manager.cpp.o" "gcc" "CMakeFiles/lightweight_comm_server.dir/src/user_manager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
